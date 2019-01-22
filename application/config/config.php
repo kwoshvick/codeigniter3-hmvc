1b0c4c25ab2877ede336ca37eb776f890193d395
kwoshvick <kwoshvick@gmail.com>
2019-01-22 15:21:08 +0300

    removed index.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_POR
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
